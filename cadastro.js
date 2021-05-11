d59e32f2a8fb38de576522dfc92ce7cc29502a42
SamStarlling <samelabarbosa.06@gmail.com>
2021-05-10 21:36:49 -0300

    adicionado o link do bootstrap nas páginas html e criada a página javascript

---
+++ cadastro.js
@@ -0,0 +1,6 @@
+document.load;
+let nome = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let senha = document.querySelector("#senha");
+let confirmacaoSenha = document.querySelector("#confirmacaoSenha");
+let cep = document.querySelector("#cep");
