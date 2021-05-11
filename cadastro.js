42e138626fe909f35bf07f9f4df98f20124660c1
SamStarlling <samelabarbosa.06@gmail.com>
2021-05-11 00:06:16 -0300

    adicionado o o evento de espera de load da página html no javascript

--- cadastro.js
+++ cadastro.js
@@ -1,25 +1,28 @@
-//Serve para limpar o valor do input quando clicado
-function limpaValorDoInput(entrada) {
-  entrada.on("click", () => {
-    entrada.val(" ");
-  });
-}
+$(document).ready(() => {
+  //Serve para limpar o valor do input quando clicado
+  function limpaValorDoInput(entrada) {
+    entrada.on("click", () => {
+      entrada.val(" ");
+    });
+  }
 
-let nome = $("#nome");
-let email = $("#email");
-let senha = $("#senha");
-let confirmacaoSenha = $("#confirmacaoSenha");
-let rg = $("#rg");
-let cep = $("#cep");
-let estado = $("#estado");
-let cidade = $("#cidade");
-let bairro = $("#bairro");
-let rua = $("#rua");
-let numero = $("#numero");
-let complemento = $("#complemento");
+  let nome = $("#nome");
+  let email = $("#email");
+  let senha = $("#senha");
+  let confirmacaoSenha = $("#confirmacaoSenha");
+  let rg = $("#rg");
+  let cep = $("#cep");
+  let estado = $("#estado");
+  let cidade = $("#cidade");
+  let bairro = $("#bairro");
+  let rua = $("#rua");
+  let numero = $("#numero");
+  let complemento = $("#complemento");
+  let btnEnviar = $("#enviar");
 
-//Limpa o valor do input passado como parâmetro;
-limpaValorDoInput(nome);
-limpaValorDoInput(email);
-limpaValorDoInput(cep);
-limpaValorDoInput(rg);
+  //Limpa o valor do input passado como parâmetro;
+  limpaValorDoInput(nome);
+  limpaValorDoInput(email);
+  limpaValorDoInput(cep);
+  limpaValorDoInput(rg);
+});
